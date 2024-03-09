5eb2b6e6f8ac3f36c889d32f6154fe43b1a40a52
Jackson Bakari <jackroot7@localhost.localdomain>
2024-03-09 17:13:42 +0300

    Auto commit made by Git script: 09/03/2024, 17:13:42

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from django.contrib import messages
 
 def get_message(request, message, type = 'info', title="Notification")->messages:
 
-    body = {"message": message, }
+    body = {"title": title, "message": message, "time": datetime.now().strftime("%H:%M:%S")}
 
     if type == 'success':
         message = messages.success(request,body)
@@ -14,5 +15,5 @@ def get_message(request, message, type = 'info', title="Notification")->messages
         message = messages.warning(request,body)
     else:
         message = messages.info(request,body)
-        
+
     return message
