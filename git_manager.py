cd59ccf1dd65f5bee255d5804b9b7787e305ec23
Jackson Bakari <jackroot7@localhost.localdomain>
2024-03-09 14:29:35 +0300

    Auto commit made by Git script: 09/03/2024, 14:29:35

--- git_manager.py
+++ git_manager.py
@@ -5,10 +5,11 @@ import time
 def commit_and_push():
     repo = git.Repo('.')  # Assuming the script is in the root directory of your Git repository
     repo.git.add('--all')
-    # commit_message = input("Enter commit message: ")
-    repo.index.commit("Auto commit made by Git script: "+ str(datetime.now().strftime("%d/%m/%Y, %H:%M:%S")))
+    commit_message = "Auto commit made by Git script: "+ str(datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    repo.index.commit(commit_message)
     origin = repo.remote(name='origin')
     origin.push('master')
+    print(commit_message)
 
 def main():
     while True:
